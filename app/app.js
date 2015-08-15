a502adda7684168c017eb3857137e8690f37b373
askl56 <amscott@openmailbox.org>
2015-08-15 21:04:31 +0100

    added user model

--- app/app.js
+++ app/app.js
@@ -3,7 +3,18 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 import inheritance from './inheritance';
-import numericBox from './numberic_box'
+import numericBox from './numberic_box';
+import user from './user';
+
+myUser = user.create({
+  firstName: "Andrew",
+  lastName: "Scott"
+});
+console.log(myUser.get('fullName')); // Andrew Scott
+
+myUser.set("firstName","Tony");
+myUser.set("lastName","Stark");
+console.log(myUser.get('fullName'));
 
 var textbox = numericBox.create();
 console.log(textbox.valid("12")); //true
