b3048c9410d1b967110384b72fbfbd4f868179d4
askl56 <amscott@openmailbox.org>
2015-08-15 17:55:18 +0100

    updated inheritance

--- app/inheritance.js
+++ app/inheritance.js
@@ -1,27 +1,25 @@
 import Ember from 'ember';
 
 export default function(){
-  // Using Plain JavaScript
-  console.log("using plain JavaScript");
-  var base = {
-    baseProperty: true
-  };
-  var derived = Object.create(base);
-  console.log(derived.baseProperty);
 
-
-  // Using Ember.js
-  console.log("using Ember.js");
   var base = Ember.Object.extend({
     baseProperty: true
   });
 
   var derived = base.extend({
-    derivedProperty:false
   });
 
-  var derivedObject = derived.create();
+  derived.reopen({
+    derivedProperty: true
+  });
 
-  console.log(derivedObject.get('baseProperty'));
-  console.log(derivedObject.get('derivedProperty'));
+  var derivedObject = derived.create({
+  });
+
+  console.log(derivedObject.get('baseProperty'));//true
+  console.log(derivedObject.get('derivedProperty'));//true
+
+  var anotherDerivedObject = derived.create();
+  console.log(anotherDerivedObject.get('derivedProperty'));//true
+
 }
